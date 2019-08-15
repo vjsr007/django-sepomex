d2e44df09807b7fb717c2b6cb27f3645ac912bb9
Víctor J. Sánchez <vjsanchezr@hotmail.com>
2019-08-14 23:12:21 -0500

    django-rest

--- pythonsite/urls.py
+++ pythonsite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('sepomex/', include('sepomex.urls')),
+    path('', include('sepomex.urls')),
     path('admin/', admin.site.urls),
 ]
