8bf535fe4fa7dd770176e3ff321d563e4456ecc2
TH658FY <victor.sanchez@ey.com>
2019-09-02 15:21:42 -0500

    .

--- tensorflow/basic_simple.py
+++ tensorflow/basic_simple.py
@@ -80,7 +80,7 @@ predictions = model.predict(test_images)
 # Plot the first X test images, their predicted label, and the true label
 # Color correct predictions in blue, incorrect predictions in red
 num_rows = 5
-num_cols = 4
+num_cols = 3
 num_images = num_rows*num_cols
 plt.figure(figsize=(2*2*num_cols, 2*num_rows))
 for i in range(num_images):
