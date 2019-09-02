a13becd3c8094cee201dd7f2c454c44440dd3b53
TH658FY <victor.sanchez@ey.com>
2019-09-02 13:37:25 -0500

    .

--- tensorflow/basic_simple.py
+++ tensorflow/basic_simple.py
@@ -6,9 +6,10 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Show image in plot with true label vs predicted label
 def plot_image(i, predictions_array, true_label, img):
   predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
-  plt.grid(False)
+  plt.grid(True)
   plt.xticks([])
   plt.yticks([])
   
@@ -25,9 +26,10 @@ def plot_image(i, predictions_array, true_label, img):
                                 class_names[true_label]),
                                 color=color)
 
+# Show bars with predictions
 def plot_value_array(i, predictions_array, true_label):
   predictions_array, true_label = predictions_array[i], true_label[i]
-  plt.grid(False)
+  plt.grid(True)
   plt.xticks([])
   plt.yticks([])
   thisplot = plt.bar(range(10), predictions_array, color="#777777")
@@ -37,35 +39,48 @@ def plot_value_array(i, predictions_array, true_label):
   thisplot[predicted_label].set_color('red')
   thisplot[true_label].set_color('blue')
 
+# Get Fashion DataSet
 fashion_mnist = keras.datasets.fashion_mnist
 
+# Download DataSet
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+# Labels
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+# Get values between 0 to 1 for Train Images
 train_images = train_images / 255.0
 
+# Get values between 0 to 1 for Test Images
 test_images = test_images / 255.0
 
+# Create Model
+# Flatten: Input Images 28x28 to linear array(one dimention)
+# ReLU: Rectified Linear Unit activation function, lineal tensor with 128 positive values or 128 neurons
+# Softmax: Ten outputs with SUM equals to 1, MAX value is the predicted label
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
+# Adam optimizer - changes weight values
+# Categorical CrossEntropy it's used when with classifification problems, each result is only a label
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
+# Trainig model with train images and their labels with five epochs
 model.fit(train_images, train_labels, epochs=5)
 
+# Made prediction with test images
 predictions = model.predict(test_images)
 
 # Plot the first X test images, their predicted label, and the true label
 # Color correct predictions in blue, incorrect predictions in red
 num_rows = 5
-num_cols = 3
+num_cols = 4
 num_images = num_rows*num_cols
 plt.figure(figsize=(2*2*num_cols, 2*num_rows))
 for i in range(num_images):
