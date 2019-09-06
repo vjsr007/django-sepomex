33e30f782c698a7f49e11137238694f4f568348e
TH658FY <victor.sanchez@ey.com>
2019-09-06 16:57:10 -0500

    .

--- tensorflow/classification_fashion.py
+++ tensorflow/classification_fashion.py
@@ -10,6 +10,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+from keras.preprocessing import image
 
 # Show image in plot with true label vs predicted label
 def plot_image(i, predictions_array, true_label, img):
@@ -46,8 +47,8 @@ def plot_value_array(i, predictions_array, true_label):
 
 def load_image(pathImg) :
     data = []
-    full_size_image = cv2.imread(pathImg, 0)
-    data.append(cv2.resize(full_size_image, (28,28), interpolation=cv2.INTER_CUBIC))
+    full_size_image = cv2.imread(pathImg, 1)
+    data.append(cv2.resize(full_size_image, (28,28), Image.ANTIALIAS))
     return data
 
 # Get Fashion DataSet
@@ -57,10 +58,15 @@ fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
 # Test Image
-image = np.asarray(load_image('tensorflow/images/shirt01.jpg'))
+data = []
+img = image.load_img('tensorflow/images/sneaker01.jpg', target_size=(28,28,1), grayscale=True)
+img = image.img_to_array(img)
+img = img/255
+data.append(img)
+train_image = np.array(data)
+train_image = train_image.mean(axis=3)
 
-# print(image.shape)
-# exit()
+print(train_image.shape)
 
 # Labels
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
@@ -92,14 +98,18 @@ model.compile(optimizer='adam',
 model.fit(train_images, train_labels, epochs=5)
 
 # Made prediction with test images
-predictions = model.predict(image)
+predictions = model.predict(train_image)
 
 print(predictions)
 
 i = 0
 plt.figure(figsize=(6,3))
 plt.subplot(1,2,1)
-plot_image(i, predictions, test_labels, image)
+plot_image(i, predictions, test_labels, train_image)
 plt.subplot(1,2,2)
 plot_value_array(i, predictions,  test_labels)
 plt.show()
+
+# https://www.analyticsvidhya.com/blog/2019/01/build-image-classification-model-10-minutes/
+# https://becominghuman.ai/building-an-image-classifier-using-deep-learning-in-python-totally-from-a-beginners-perspective-be8dbaf22dd8
+# https://medium.com/datadriveninvestor/deep-learning-techniques-for-text-classification-9392ca9492c7
