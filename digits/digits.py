c78b783521adf00fe2c24e09cf73b0b86e016a5b
TH658FY <victor.sanchez@ey.com>
2019-09-13 09:39:48 -0500

    .

--- digits/digits.py
+++ digits/digits.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+from tensorflow import keras
 
 # print(os.listdir("digits/input"))
 
@@ -14,7 +15,7 @@ FAST_RUN=True
 batch_size=32
 epochs=100
 if FAST_RUN:
-    epochs=1
+    epochs=5
 
 train_data = pd.read_csv("digits/input/train.csv")
 test_data = pd.read_csv("digits/input/test.csv")
@@ -69,24 +70,24 @@ validation_generator = validation_datagen.flow(
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 
-model = Sequential()
-model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28,28,1)))
-model.add(Conv2D(32, kernel_size=3, activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(10, activation='softmax'))
-
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-
-from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
-
-callbacks = [
-    EarlyStopping(patience=10, verbose=1),
-    ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.00001, verbose=1),
-    ModelCheckpoint('digits/model.h5', verbose=1, save_best_only=True, save_weights_only=True)
-]
-
 if TRAIN:
+    model = Sequential()
+    model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28,28,1)))
+    model.add(Conv2D(32, kernel_size=3, activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(10, activation='softmax'))
+
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+
+    callbacks = [
+        EarlyStopping(patience=10, verbose=1),
+        ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.00001, verbose=1),
+        ModelCheckpoint('digits/model.h5', verbose=1, save_best_only=True, save_weights_only=True)
+    ]
+
     model.fit_generator(
         train_generator, 
         steps_per_epoch=len(x_train) // batch_size, 
@@ -95,14 +96,14 @@ if TRAIN:
         epochs=epochs,
         callbacks=callbacks
     )
-    model.save_weights("digits/model.h5")
+    model.save("digits/model.h5")
 
     x_test_recaled = (x_test.astype("float32") / 255)
     scores = model.evaluate(x_test_recaled, y_test, verbose=0)
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
     print("%s: %.2f%%" % (model.metrics_names[0], scores[0]*100))    
 else:
-    model.load_weights("digits/model.h5")
+    model = keras.models.load_model("digits/model.h5")
 
 test_digit_data = test_data.values.reshape(test_data.shape[0],28,28,1).astype("float32") / 255
 predictions = model.predict(test_digit_data)
