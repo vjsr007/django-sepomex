5c146debb470c429f5729ef63fd18680a135e8fb
TH658FY <victor.sanchez@ey.com>
2019-09-17 15:53:19 -0500

    .

--- pix2pix/pix2pix.py
+++ pix2pix/pix2pix.py
@@ -2,6 +2,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+# Disable warning
+import warnings
+warnings.filterwarnings('ignore')
+
 import tensorflow as tf
 import numpy as np
 import argparse
