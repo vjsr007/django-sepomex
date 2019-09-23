d9a3ca891c58adeee9442bcaf180fc27cbeda50d
TH658FY <victor.sanchez@ey.com>
2019-09-23 14:21:35 -0500

    .

--- sepomex/view/auth.py
+++ sepomex/view/auth.py
@@ -19,7 +19,7 @@ def get_token(request):
             })
 
 class CustomAuthToken(ObtainAuthToken):
-    def get(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
                                            context={'request': request})
         serializer.is_valid(raise_exception=True)
